0b2e21ab3cf22e5ecdfc4765ca27d0137458a809
vfeofilaktov <vfeofilaktov@ptsecurity.com>
2022-11-18 01:43:45 +0300

    Update Main.kt

--- platform/src/main/kotlin/me/vldf/lsl/extractor/platform/Main.kt
+++ platform/src/main/kotlin/me/vldf/lsl/extractor/platform/Main.kt
@@ -1,11 +1,13 @@
 package me.vldf.lsl.extractor.platform
 
 import me.vldf.lsl.jvm.reader.JvmClassReader
+import me.vldf.lsl.stages.assign.AssignExtractor
 
 fun main() {
     val config = PipelineConfig {
         libraryPath = "./testData/build/classes/java/main/"
         stages.add(JvmClassReader())
+        stages.add(AssignExtractor())
     }
 
     AnalysisPipeline(config).run()
