8c3808170bb540ed604b325726d5e6b05177f748
vfeofilaktov <vfeofilaktov@ptsecurity.com>
2023-05-01 22:07:02 +0300

    Fix predicate states filtering

--- stages/exceptions-extractor/src/main/kotlin/me/vldf/lsl/stages/exceptions/predicate/state/utils/PredicateStateHelper.kt
+++ stages/exceptions-extractor/src/main/kotlin/me/vldf/lsl/stages/exceptions/predicate/state/utils/PredicateStateHelper.kt
@@ -1,12 +1,16 @@
 package me.vldf.lsl.stages.exceptions.predicate.state.utils
 
+import org.jetbrains.research.kex.state.BasicState
 import org.jetbrains.research.kex.state.ChainState
 import org.jetbrains.research.kex.state.PredicateStateWithPath
 import org.jetbrains.research.kex.state.transformer.Optimizer
 
 object PredicateStateHelper {
     fun shouldPredicateStateBeInSpecification(predicate: PredicateStateWithPath): Boolean {
-        // todo: пропускаются некоторые PS, которые не должны пропускаться (мб вида true = true?)
-        return Optimizer.apply(predicate.toPredicateState()) is ChainState
+        return when(val optimizedState = Optimizer.apply(predicate.toPredicateState())) {
+            is ChainState -> true
+            is BasicState -> optimizedState.evaluatesToTrue
+            else -> false
+        }
     }
 }
